5b92c767eb1139b9cdf8c1e29107a5a3d5caf138
Yue She <yueshe1996@gmail.com>
2016-03-17 04:49:25 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :caldining
-  root :to => redirect('/dining')
+  root :to => redirect('/caldining')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
