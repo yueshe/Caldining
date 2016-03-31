51c3474679beba09a5afae46742b642600b1ba72
Yang Yu <yangyu@berkeley.edu>
2016-03-31 01:53:56 +0000

    admin system finished

--- app/controllers/halls_controller.rb
+++ app/controllers/halls_controller.rb
@@ -1,6 +1,6 @@
 class HallsController < ApplicationController
   before_action :set_hall, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:show, :index]  
   # GET /halls
   # GET /halls.json
   def index
@@ -15,18 +15,27 @@ class HallsController < ApplicationController
 
   # GET /halls/new
   def new
+    if self.auth_admin
+      return 
+    end
     @hall = Hall.new
   end
 
   # GET /halls/1/edit
   def edit
+    if self.auth_admin
+      return 
+    end
   end
 
   # POST /halls
   # POST /halls.json
   def create
+    if self.auth_admin
+      return 
+    end
     @hall = Hall.new(hall_params)
-
+    
     respond_to do |format|
       if @hall.save
         format.html { redirect_to @hall, notice: 'Hall was successfully created.' }
@@ -41,6 +50,9 @@ class HallsController < ApplicationController
   # PATCH/PUT /halls/1
   # PATCH/PUT /halls/1.json
   def update
+    if self.auth_admin
+      return 
+    end
     respond_to do |format|
       if @hall.update(hall_params)
         format.html { redirect_to @hall, notice: 'Hall was successfully updated.' }
@@ -55,12 +67,23 @@ class HallsController < ApplicationController
   # DELETE /halls/1
   # DELETE /halls/1.json
   def destroy
+    if self.auth_admin
+      return 
+    end
     @hall.destroy
     respond_to do |format|
       format.html { redirect_to halls_url, notice: 'Hall was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
+
+    def auth_admin
+      if !current_user || !current_user.admin
+        redirect_to halls_path, :flash => { :error => 'You are not admin' }
+        return true
+      end
+      return false
+    end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -73,4 +96,5 @@ class HallsController < ApplicationController
     def hall_params
       params.require(:hall).permit(:name)
     end
+    
 end
