68590cad3d67b2ef76c0be885076787e664a9161
Yang Yu <yangyu@berkeley.edu>
2016-04-15 01:04:55 +0000

    DRY out javascript

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -28,6 +28,11 @@ var Add_to_diet = {
   setup: function() {
      $(".add").click(function(e){
        var amount = $(".quantity",this.parentNode).val()
+       if (amount == "") {
+         alert("Please enter an valid amount")
+         e.stopImmediatePropagation();
+         return
+       }
        e.stopImmediatePropagation();
      });
   }
