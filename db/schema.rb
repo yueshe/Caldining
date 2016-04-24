93c1f326659643685425544b80217c3dcdb37bf0
yihelu <yihelu@berkeley.edu>
2016-04-24 21:53:09 +0000

    revert

--- db/schema.rb
+++ db/schema.rb
@@ -13,24 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160424023301) do
 
-  create_table "admins", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
-
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
-  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-
   create_table "halls", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -50,7 +32,7 @@ ActiveRecord::Schema.define(version: 20160424023301) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.boolean  "nutrition_available", default: false
-    t.integer  "calories_from_fat",   default: 0
+    t.integer  "calories_from_fat"
     t.string   "mealtime"
   end
 
