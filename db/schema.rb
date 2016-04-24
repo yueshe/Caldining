b67a083526ab1488db366643e7825e2d44f7b007
yueshe <yueshe1996@gmail.com>
2016-04-24 07:20:05 +0000

    db

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,45 @@
 
 ActiveRecord::Schema.define(version: 20160420214500) do
 
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+
   create_table "halls", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "intakes", force: :cascade do |t|
+    t.datetime "date"
+    t.string   "meal"
+    t.decimal  "calories"
+    t.decimal  "fat"
+    t.decimal  "carb"
+    t.decimal  "cholesterol"
+    t.decimal  "protein"
+    t.decimal  "sodium"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
+
+  add_index "intakes", ["user_id"], name: "index_intakes_on_user_id"
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
