6bacbe30ddc15ab782f4907f213ce328a8cd117a
Alina Vuong <alvuong@berkeley.edu>
2016-04-25 03:59:53 +0000

    changes integrated, search fixed, css changes removed

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -10,12 +10,13 @@ var Search = {
     $( "#search" ).keyup(function(){
       $("tr.food").each(function() {
         var target = $('#search').val().replace(/ /g,'').toLowerCase()
-        var food_name = $("td.name", this).html().replace(/ /g,'').toLowerCase()
-        var food_date = $("td.date", this).html().replace(/ /g,'').toLowerCase()
-        var food_location = $("td.location", this).html().replace(/ /g,'').toLowerCase()
-        if (food_name.includes(target) || food_location == target || food_date == target) {
+        var food_name = $("td.name", this).text().replace(/ /g,'').toLowerCase()
+        var food_date = $("td.mealtime", this).html().replace(/ /g,'').toLowerCase()
+        var food_location = $("td.location", this).text().replace(/ /g,'').toLowerCase()
+        if (food_name.includes(target) || food_location.includes(target) || food_date.includes(target)) {
           $(this).show()
         } else {
+          console.log("hiding")
           $(this).hide()
         }
       });
@@ -23,18 +24,3 @@ var Search = {
   }
 };
 $(Search.setup);
-
-var Add_to_diet = {
-  setup: function() {
-     $(".add").click(function(e){
-       var amount = $(".quantity",this.parentNode).val()
-       if (amount == "") {
-         alert("Please enter an valid amount")
-         e.stopImmediatePropagation();
-         return
-       }
-       e.stopImmediatePropagation();
-     });
-  }
-};
-$(Add_to_diet.setup);
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,3 @@
 class Item < ActiveRecord::Base
-    has_and_belongs_to_many :users, counter_cache: :servings
+    has_and_belongs_to_many :users
 end
