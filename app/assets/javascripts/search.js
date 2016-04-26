ae5e72ecd7756ed72f496b4327fdfd0ac20d4511
Yang Yu <yangyu@berkeley.edu>
2016-04-26 01:04:23 +0000

    correct counts for add in user profile

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -13,11 +13,7 @@ var Search = {
         var food_name = $("td.name", this).text().replace(/ /g,'').toLowerCase()
         var food_date = $("td.mealtime", this).html().replace(/ /g,'').toLowerCase()
         var food_location = $("td.location", this).text().replace(/ /g,'').toLowerCase()
-<<<<<<< HEAD
         if (food_name.includes(target) || food_location == target || food_date == target) {
-=======
-        if (food_name.includes(target) || food_location.includes(target) || food_date.includes(target)) {
->>>>>>> 400f91e838f79b481e6b5b41f37cbc2c57a29eaf
           $(this).show()
         } else {
           console.log("hiding")
@@ -27,9 +23,5 @@ var Search = {
     });
   }
 };
-<<<<<<< HEAD
 $(Search.setup);
 
-=======
-$(Search.setup);
->>>>>>> 400f91e838f79b481e6b5b41f37cbc2c57a29eaf
