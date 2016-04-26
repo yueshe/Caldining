0578913db65e599651797380478613dafd73cd52
Yang Yu <yangyu@berkeley.edu>
2016-04-26 19:00:38 +0000

    improved page layout and UX

--- app/assets/javascripts/draw.js
+++ app/assets/javascripts/draw.js
@@ -1,11 +1,19 @@
+var curr_chart;
+var food_list;
+var calories_list;
+var color_list;
+var fat_list;
+var carb_list;
+
 var draw = {
   setup: function() {
-    
-    var food_list = []
-    var calories_list = []
-    var color_list = []
-    var fat_list = []
-    var carb_list = []
+    //Data intiation
+    food_list = []
+    calories_list = []
+    color_list = []
+    fat_list = []
+    carb_list = []
+    //Data retreival
     $("tr.food").each(function() {
      food_list.push($("td.name", this).text())
      calories_list.push(parseFloat($("td.calories", this).html()) * parseFloat($("td.servings", this).html()))
@@ -13,53 +21,61 @@ var draw = {
      fat_list.push(parseFloat($("td.fat", this).html()) * parseFloat($("td.servings", this).html()))
      color_list.push('#'+Math.floor(Math.random()*16777215).toString(16));
     });
-    console.log(food_list)
-        console.log(calories_list)
-            console.log(color_list)
-    var ctx = $("#calories_chart");
-    var ctx2 = $("#fat_chart");
-    var ctx3 = $("#carb_chart");
-    var calories_chart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-    labels: food_list,
-    datasets: [
-        {
-            data: calories_list,
-            backgroundColor: color_list,
-            hoverBackgroundColor: color_list
-        }]
-    }
-});
-var fat_chart = new Chart(ctx2, {
-    type: 'doughnut',
-    data: {
-    labels: food_list,
-    datasets: [
-        {
-            data: fat_list,
-            backgroundColor: color_list,
-            hoverBackgroundColor: color_list
-        }]
-    }
-});
- var carb_chart = new Chart(ctx3, {
-    type: 'doughnut',
-    data: {
-    labels: food_list,
-    datasets: [
-        {
-            data: carb_list,
-            backgroundColor: color_list,
-            hoverBackgroundColor: color_list
-        }]
-    }
-});
+    //Initial set up
+    chart("#calories_chart", food_list, calories_list, color_list)
+    chart("#fat_chart", food_list, fat_list, color_list)
+    chart("#carb_chart", food_list, carb_list, color_list)
 
-
-
-
-
+    $("#fat_div").hide()
+    $("#carb_div").hide()
+    $("#total_calories").text("Total Calories: " + calories_list.reduce(add, 0) + "KCal");
+    $("#total_fat").text("Total Fat: " + fat_list.reduce(add, 0) + "g");
+    $("#total_carb").text("Total Carbs: " + carb_list.reduce(add, 0) + "g");
   }
 };
 $(draw.setup);
+
+
+var chart = function(canvas, name, data, color) {
+    var ctx = $(canvas);
+    var chart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+    labels: name,
+    datasets: [
+        {
+            data: data,
+            backgroundColor: color,
+            hoverBackgroundColor: color
+        }]
+    }
+});
+    return chart
+}
+
+var show = {
+  setup: function() {
+    $("#chart_picker").change(function() {
+        var chart_type = $("#chart_picker").val()
+        if (chart_type == "calories") {
+            $("#calories_div").fadeIn()
+            $("#fat_div").hide()
+            $("#carb_div").hide()
+        }   else if (chart_type == "fat") {
+            $("#fat_div").fadeIn()
+            $("#calories_div").hide()
+            $("#carb_div").hide()
+        } else if (chart_type == "carbs") {
+            $("#carb_div").fadeIn()
+            $("#calories_div").hide()
+            $("#fat_div").hide()
+        }
+    });
+    
+  }
+};
+$(show.setup);
+
+function add(a, b) {
+    return a + b;
+}
