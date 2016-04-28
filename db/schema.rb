51bcc3bb8b4d77481f97e0b652c40475e3d03bfa
Alina Vuong <alvuong@berkeley.edu>
2016-04-28 22:57:05 +0000

    rename_column typo fixed again.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160427083139) do
+ActiveRecord::Schema.define(version: 20160428225308) do
 
   create_table "halls", force: :cascade do |t|
     t.string   "name"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20160427083139) do
     t.decimal  "protein",             default: 0.0
     t.decimal  "sodium",              default: 0.0
     t.string   "location"
-    t.date     "timeframe"
+    t.date     "date"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.boolean  "nutrition_available", default: false
