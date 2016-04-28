1fb7c1aeb7b0b35f1b3192716f8db8e78dc5ec89
Yang Yu <yangyu@berkeley.edu>
2016-04-28 21:21:28 +0000

    modify title bar

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,21 @@
 
 ActiveRecord::Schema.define(version: 20160427083139) do
 
-  create_table "add_total_to_servings", force: :cascade do |t|
-    t.integer  "total"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
-<<<<<<< HEAD
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
 
@@ -28,13 +36,27 @@ ActiveRecord::Schema.define(version: 20160427083139) do
     t.datetime "updated_at", null: false
   end
 
-=======
->>>>>>> 56313093b0ab4a9327dcf7a98c6d5de7d503a7a8
   create_table "halls", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "intakes", force: :cascade do |t|
+    t.datetime "date"
+    t.string   "meal"
+    t.decimal  "calories"
+    t.decimal  "fat"
+    t.decimal  "carb"
+    t.decimal  "cholesterol"
+    t.decimal  "protein"
+    t.decimal  "sodium"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
+
+  add_index "intakes", ["user_id"], name: "index_intakes_on_user_id"
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -45,23 +67,13 @@ ActiveRecord::Schema.define(version: 20160427083139) do
     t.decimal  "protein",             default: 0.0
     t.decimal  "sodium",              default: 0.0
     t.string   "location"
-    t.string   "date"
+    t.date     "timeframe"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.boolean  "nutrition_available", default: false
     t.integer  "calories_from_fat",   default: 0
     t.string   "mealtime"
   end
-
-  create_table "serving", force: :cascade do |t|
-    t.integer  "item_id"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "serving", ["item_id"], name: "index_serving_on_item_id"
-  add_index "serving", ["user_id"], name: "index_serving_on_user_id"
 
   create_table "servings", force: :cascade do |t|
     t.integer  "user_id"
